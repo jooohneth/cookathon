1fd6c4c7ae36e4b1a713d58ea2d942d245ef0866
jooohneth <jooohn.eth@gmail.com>
2025-04-28 14:39:27 -0400

    fix: responsive title

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -1,7 +1,9 @@
 const Partners = () => {
   return (
     <div className="w-full mt-12 flex flex-col gap-4">
-      <div className="text-3xl font-bold uppercase">Partners</div>
+      <div className="text-md md:text-xl lg:text-2xl xl:text-3xl font-bold uppercase">
+        Partners
+      </div>
       <div className="grid grid-cols-3 md:grid-cols-6 gap-4">
         <div className="w-full h-16 md:h-24 border-6 border-dotted border-o-accent"></div>
         <div className="w-full h-16 md:h-24 border-6 border-dotted border-o-accent"></div>
