50929fa9b3572c7e6444d0eacc9699c99d90b4f1
jooohneth <jooohn.eth@gmail.com>
2025-05-02 09:48:34 -0600

    feat: add more partners

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -6,7 +6,7 @@ const Partners = () => {
       <div className="text-md md:text-xl lg:text-2xl xl:text-3xl font-bold uppercase">
         Partners
       </div>
-      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-8 gap-y-2 md:gap-8 lg:gap-16 items-center justify-center">
+      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-8 gap-y-0 lg:gap-y-4 lg:gap-x-16 items-center justify-center">
         <div className="w-full h-16 md:h-24 flex items-center justify-center">
           <a href="https://privy.io" target="_blank" rel="noopener noreferrer">
             <Image
@@ -65,6 +65,21 @@ const Partners = () => {
         </div>
         <div className="w-full h-16 md:h-24 flex items-center justify-center">
           <a
+            href="https://www.airfoil.studio/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/airfoil.svg"
+              alt="Airfoil"
+              width={0}
+              height={0}
+              className="w-20 md:w-30"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+          <a
             href="https://www.odisea.xyz/"
             target="_blank"
             rel="noopener noreferrer"
@@ -90,6 +105,21 @@ const Partners = () => {
               width={0}
               height={0}
               className="w-40"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+          <a
+            href="https://www.developerdao.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/d_d.svg"
+              alt="DeveloperDAO"
+              width={0}
+              height={0}
+              className="w-15 lg:w-20"
             />
           </a>
         </div>
