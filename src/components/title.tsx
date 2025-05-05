2e9d7fb9b0f87255346f0b80b0a872d017224faa
jooohneth <jooohn.eth@gmail.com>
2025-05-05 14:20:10 -0600

    fix: logo

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -7,8 +7,8 @@ const Title = () => {
         <Image
           src="/cookathon.svg"
           alt="logo"
-          width={0}
-          height={0}
+          width={100}
+          height={100}
           className="w-8 h-8 md:w-12 md:h-12 lg:w-20 lg:h-20 xl:w-28 xl:h-28"
         />
         <div className="flex gap-4 md:gap-8 lg:gap-12 xl:gap-16">
