9a66b8028eb6ee960c81c0efbe4278ccb44631e7
jooohneth <jooohn.eth@gmail.com>
2025-05-05 14:25:18 -0600

    feat: separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,18 @@ import Title from "@/components/title";
 import Details from "@/components/details";
 import Partners from "@/components/partners";
 import Footer from "@/components/footer";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <div className="font-mono min-h-screen ">
-      <main className="flex-1 px-8 py-4 md:px-20 md:py-12">
+    <div className="font-mono min-h-screen">
+      <main className="flex-1 px-8 py-4 md:px-20 md:py-12 h-screen">
         <Header />
         <Title />
         <Details />
         <Partners />
       </main>
+      <Separator />
       <Footer />
     </div>
   );
