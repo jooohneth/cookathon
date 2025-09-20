6840efd42d67861c3cd224e55fb331dc8cfa9663
jooohneth <jooohn.eth@gmail.com>
2025-09-20 16:27:33 -0400

    chore: the end.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,18 +12,19 @@ const Header = () => {
           WebkitTextStroke: "1px var(--o-accent-stroke)",
         }}
       >
-        <span>Round 1 Submissions Due: {"  "}</span>
-        <Countdown targetDate={new Date("2025-08-08T23:59:59Z")} />
+        <span>THE PROGRAM HAS ENDED. {"  "}</span>
+        {/* <Countdown targetDate={new Date("2025-08-08T23:59:59Z")} /> */}
       </div>
       <div className="flex flex-row items-center gap-4">
         <Button
           size="lg"
           className="text-lg font-semibold uppercase text-foreground bg-transparent border border-foreground hover:bg-transparent hover:border-o-accent hover:text-o-accent transition-colors duration-400 cursor-pointer"
           onClick={() => {
-            window.open("https://lu.ma/hnm6xu2m", "_blank");
+            // window.open("https://lu.ma/hnm6xu2m", "_blank");
+            window.open("https://mantle-builders-hub.vercel.app", "_blank");
           }}
         >
-          Register
+          Closed
         </Button>
         {/* <ThemeToggle /> */}
       </div>
--- src/components/title.tsx
+++ src/components/title.tsx
@@ -3,20 +3,20 @@ import Image from "next/image";
 const Title = () => {
   return (
     <div className="mt-12 flex flex-col md:gap-4">
-      <div className="w-full flex items-center justify-between uppercase text-4xl md:text-6xl lg:text-8xl xl:text-9xl font-bold">
-        <Image
+      <div className="w-full flex items-center lg:justify-start uppercase text-4xl md:text-6xl lg:text-8xl xl:text-9xl font-bold">
+        {/* <Image
           src="/cookathon.svg"
           alt="logo"
           width={100}
           height={100}
           className="w-8 h-8 md:w-12 md:h-12 lg:w-20 lg:h-20 xl:w-28 xl:h-28"
-        />
+        /> */}
         <div className="flex gap-4 md:gap-8 lg:gap-12 xl:gap-16">
           <div className="">The</div>
           <div className="">Cookathon</div>
         </div>
       </div>
-      <div className="font-semibold text-right uppercase text-md md:text-xl lg:text-3xl xl:text-4xl">
+      <div className="font-semibold text-left uppercase text-md md:text-xl lg:text-3xl xl:text-4xl">
         Where builders come to cook
       </div>
     </div>
