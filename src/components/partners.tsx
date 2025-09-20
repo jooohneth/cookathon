5095cf22b1342a6752d3669512f7751b734ba4dc
jooohneth <jooohn.eth@gmail.com>
2025-09-20 17:18:28 -0400

    feat: add partners

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -6,19 +6,8 @@ const Partners = () => {
       <div className="text-md md:text-xl lg:text-2xl xl:text-3xl font-bold uppercase">
         Partners
       </div>
-      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-8 gap-y-0 lg:gap-x-16 items-center justify-center">
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
-          <a href="https://privy.io" target="_blank" rel="noopener noreferrer">
-            <Image
-              src="/partners/privy.svg"
-              alt="Privy"
-              width={0}
-              height={0}
-              className="w-35"
-            />
-          </a>
-        </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+      <div className="grid grid-cols-3 md:grid-cols-6 gap-x-8 gap-y-0 lg:gap-x-16 items-center px-10 border-1">
+        <div className="w-full h-16 md:h-24 flex items-center">
           <a
             href="https://layerzero.network/"
             target="_blank"
@@ -29,11 +18,82 @@ const Partners = () => {
               alt="LayerZero"
               width={0}
               height={0}
-              className="w-60"
+              className="w-40"
             />
           </a>
         </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center mb-1 lg:mb-2">
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://stargate.finance"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/stargate.svg"
+              alt="Stargate"
+              width={0}
+              height={0}
+              className="w-40"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a href="https://li.fi" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/partners/lifi.svg"
+              alt="Li.Fi"
+              width={0}
+              height={0}
+              className="w-30"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://hyperlane.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/hyperlane.svg"
+              alt="Hyperlane"
+              width={0}
+              height={0}
+              className="w-52"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://www.squidrouter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/squid.svg"
+              alt="Squid"
+              width={0}
+              height={0}
+              className="w-30"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://omni.network/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/omni.svg"
+              alt="Omni"
+              width={0}
+              height={0}
+              className="w-35 mr-10"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center mb-1 lg:mb-2">
           <a
             href="https://zerion.io/api"
             target="_blank"
@@ -48,22 +108,44 @@ const Partners = () => {
             />
           </a>
         </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
-          <a
-            href="https://omni.network/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a href="https://privy.io" target="_blank" rel="noopener noreferrer">
             <Image
-              src="/partners/omni.svg"
-              alt="Omni"
+              src="/partners/privy.svg"
+              alt="Privy"
               width={0}
               height={0}
               className="w-35"
             />
           </a>
         </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://dynamic.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/dynamic.svg"
+              alt="Dynamic"
+              width={0}
+              height={0}
+              className="w-40"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a href="https://reown.com" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/partners/reown.svg"
+              alt="Reown"
+              width={0}
+              height={0}
+              className="w-52"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
           <a
             href="https://www.thirdweb.com/"
             target="_blank"
@@ -78,7 +160,23 @@ const Partners = () => {
             />
           </a>
         </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://www.poap.fun/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/poap.svg"
+              alt="POAP"
+              width={0}
+              height={0}
+              className="w-30 mb-1"
+            />
+          </a>
+        </div>
+
+        <div className="w-full h-16 md:h-24 flex items-center">
           <a
             href="https://www.airfoil.studio/"
             target="_blank"
@@ -89,11 +187,12 @@ const Partners = () => {
               alt="Airfoil"
               width={0}
               height={0}
-              className="w-20 md:w-25"
+              className="w-30"
             />
           </a>
         </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+
+        <div className="w-full h-16 md:h-24 flex items-center">
           <a
             href="https://www.odisea.xyz/"
             target="_blank"
@@ -108,7 +207,7 @@ const Partners = () => {
             />
           </a>
         </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
+        <div className="w-full h-16 md:h-24 flex items-center">
           <a
             href="https://www.talentprotocol.com/"
             target="_blank"
@@ -125,36 +224,6 @@ const Partners = () => {
         </div>
         <div className="w-full h-16 md:h-24 flex items-center justify-center">
           <a
-            href="https://www.growthepie.xyz/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/partners/growthepie.svg"
-              alt="GrowThePie"
-              width={0}
-              height={0}
-              className="mb-1 w-100"
-            />
-          </a>
-        </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
-          <a
-            href="https://www.poap.fun/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/partners/poap.svg"
-              alt="POAP"
-              width={0}
-              height={0}
-              className="w-15 md:w-20 mb-1"
-            />
-          </a>
-        </div>
-        <div className="w-full h-16 md:h-24 flex items-center justify-center">
-          <a
             href="https://www.developerdao.com/"
             target="_blank"
             rel="noopener noreferrer"
@@ -165,6 +234,21 @@ const Partners = () => {
               width={0}
               height={0}
               className="w-15 lg:w-20"
+            />
+          </a>
+        </div>
+        <div className="w-full h-16 md:h-24 flex items-center">
+          <a
+            href="https://www.growthepie.xyz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/partners/growthepie.svg"
+              alt="GrowThePie"
+              width={0}
+              height={0}
+              className="mb-1 w-100"
             />
           </a>
         </div>
